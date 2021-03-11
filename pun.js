1d7b72255193838366680a4d9f443e03fdfa71fa
Chris Grimmett <chris@grimtech.net>
2021-03-10 19:53:57 -0800

    fix formatting

--- pun.js
+++ pun.js
@@ -1,9 +1,14 @@
 let littlePuns = require('./puns/little-pun-book-the.json');
 let dadPuns = require('./puns/dad.json');
+// let testPuns = require('./puns/test.json');
 let puns = littlePuns.concat(dadPuns);
+let md = require('markdown-it')();
+
+
 
 let randomPun = () => {
-	return puns[Math.floor(Math.random() * puns.length)];
+	const punSelection = puns[Math.floor(Math.random() * puns.length)];
+	return md.render(punSelection, { breaks: true });
 }
 
 module.exports = {
